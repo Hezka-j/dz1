644d1ad594c3a2e82d218b906de44b87fce3427b
Hezka-j <145163687+Hezka-j@users.noreply.github.com>
2023-09-21 22:18:06 +0300

    Add files via upload

---
+++ dz1/Head.h
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<ctime>
+#include<conio.h>
+#include<windows.h>
+using namespace std;
+enum Colors { BLACK, DARKBLUE, DARKGREEN, DARKCYAN, DARKRED, DARKMAGENTA, DARKYELLOW, GREY, DARKGREY, BLUE, GREEN, CYAN, RED, MAGENTA, YELLOW, WHITE };
---
+++ dz1/dz1.cpp
@@ -0,0 +1,25 @@
+﻿#include "Head.h"
+void DrawLine(int, string, Colors, bool);
+int main()
+{
+    DrawLine(20, "@", GREEN, false);
+}
+
+void DrawLine(int count, string t, Colors color, bool c)
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, color);
+    for (int i = 0; i < count; i++)
+    {
+        if (c == true)
+        {
+            cout << t;
+        }
+        else
+        {
+            cout << t << "\n";
+        }
+    }
+        
+    Sleep(2000);
+}
